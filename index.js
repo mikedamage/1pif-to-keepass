ed008d48bf4106feadc95f4085c8cb7ebf9ca772
Mike Green <mike.is.green@gmail.com>
2015-06-10 14:26:23 -0400

    Added data and stream end handler modules, created XML starter file to be loaded by the converter

--- index.js
+++ index.js
@@ -2,12 +2,15 @@
 
 'use strict';
 
-var _          = require('lodash');
-var fs         = require('fs');
-var path       = require('path');
-var util       = require('util');
-var events     = require('events');
-var jsonStream = require('./lib/json-stream');
+var _           = require('lodash');
+var fs          = require('fs');
+var path        = require('path');
+var util        = require('util');
+var events      = require('events');
+var DomParser   = require('xmldom').DomParser;
+var jsonStream  = require('./lib/json-stream');
+var dataHandler = require('./lib/data-handler');
+var endHandler  = require('./lib/end-handler');
 
 var defaults = {
   output: process.stdout
@@ -18,6 +21,7 @@ var OnePassConverter = function OnePassConverter(input, options) {
   this.input       = input;
   this.options     = _.assign(defaults, options);
   this.inputStream = fs.createReadStream(this.input);
+  this.entries     = {};
 
   events.EventEmitter.call(this);
 };
@@ -27,10 +31,12 @@ util.inherits(OnePassConverter, events.EventEmitter);
 OnePassConverter.prototype.start = function() {
   this.emit('start', this.input, this.options);
 
-  var self        = this;
-  this.jsonStream = jsonStream(this.inputStream).on('data', function(data) {
-    self.emit('json', data);
-  });
+  fs.readFile(path.join(__dirname, 'xml-starter.xml'), _.bind(function(xml) {
+    this.xml        = new DomParser().parseFromString(xml);
+    this.jsonStream = jsonStream(this.inputStream)
+      .on('data', _.bind(dataHandler, this))
+      .on('end', _.bind(endHandler, this));
+  }, this));
 
   return true;
 };
