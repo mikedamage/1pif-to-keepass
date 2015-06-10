d0e50585103a9ed1eb3e74b52cdeb3a19cc431ce
Mike Green <mike.is.green@gmail.com>
2015-06-10 17:39:28 -0400

    replaced xmldom with Cheerio

--- index.js
+++ index.js
@@ -7,12 +7,10 @@ var fs            = require('fs');
 var path          = require('path');
 var util          = require('util');
 var events        = require('events');
-var xmldom        = require('xmldom');
 var jsonStream    = require('./lib/json-stream');
+var cheerio       = require('cheerio');
 var dataHandler   = require('./lib/data-handler');
 var endHandler    = require('./lib/end-handler');
-var DOMParser     = xmldom.DOMParser;
-var XMLSerializer = xmldom.XMLSerializer;
 
 var defaults = {
   output: process.stdout
@@ -24,8 +22,6 @@ var OnePassConverter = function OnePassConverter(input, options) {
   this.options     = _.assign(defaults, options);
   this.inputStream = fs.createReadStream(this.input);
   this.entries     = [];
-  this.xmlParser   = new DOMParser();
-  this.serializer  = new XMLSerializer();
 
   events.EventEmitter.call(this);
 };
@@ -38,8 +34,7 @@ OnePassConverter.prototype.start = function() {
   fs.readFile(path.join(__dirname, 'xml-starter.xml'), _.bind(function(err, xml) {
     if (err) throw err;
 
-    this.xml        = this.xmlParser.parseFromString(xml.toString());
-    this.xmlRoot    = this.xml.documentElement.getElementsByTagName('root')[0];
+    this.xml        = cheerio.load(xml.toString());
     this.jsonStream = jsonStream(this.inputStream)
       .on('data', _.bind(dataHandler, this))
       .on('end', _.bind(endHandler, this));
